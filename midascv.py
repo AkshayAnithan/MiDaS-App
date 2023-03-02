367de935180b0b54b8761f3c10523cdbd9c85b9d
akshayarjundeepu <akshayarjundeepu@gmail.com>
2023-03-02 12:30:36 +0530

    Phase 1

---
+++ midascv.py
@@ -0,0 +1,48 @@
+import cv2
+import torch
+import matplotlib.pyplot as plt
+
+# Clear cache
+#torch.hub.clear_cache()
+
+#Download the MiDaS
+midas = torch.hub.load('intel-isl/MiDaS', 'MiDaS_small')
+midas.to('cpu')
+midas.eval()
+
+# Input tranformational pipeline
+transforms = torch.hub.load('intel-isl/MiDaS','transforms')
+transform = transforms.small_transform
+
+# Hook into OpenCV
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    #Transform input for midas
+    img = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    imgbatch = transform(img).to('cpu')
+    
+    # Make a prediction
+    with torch.no_grad():
+        prediction = midas(imgbatch)
+        prediction = torch.nn.functional.interpolate(
+            prediction.unsqueeze(1),
+            size = img.shape[:2],
+            mode = 'bicubic',
+            align_corners = False
+        ).squeeze()
+        
+        output = prediction.cpu().numpy()
+        
+        print(output)
+    
+    plt.imshow(output)
+    cv2.imshow('CV2Frame',frame)
+    plt.pause(0.00001)
+    
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+        
+plt.show()
